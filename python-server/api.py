f46d86737d26452bd243f0c16cc102dd4bf16be0
Khushaal Choithramani <73185436+khushxxl@users.noreply.github.com>
2024-10-15 11:10:51 +0100

    Add files via upload

---
+++ python-server/api.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify # type: ignore
+
+app = Flask(__name__)
+allowed_mac_addrs = ["08:97:98:b2:7e:83"]
+
+@app.route('/mac', methods=['POST'])
+def receive_mac_address():
+    data = request.json
+    mac_address = data.get('mac_address')
+    if mac_address in allowed_mac_addrs:
+        return jsonify("TRUE"), 200
+    return jsonify("FALSE"), 403
+
+# For Vercel to recognize the app
+if __name__ == "__main__":
+    app.run()
